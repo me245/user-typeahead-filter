9b19da69fbaeec0350655d57e6ae2d5726ba1d86
Michael Evanson <michael.evanson@gmail.com>
2018-03-10 22:47:56 -0700

    Add grid template and user input

--- src/app/user-grid/user-grid.component.ts
+++ src/app/user-grid/user-grid.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../data-models/user';
 
 @Component({
   selector: 'app-user-grid',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-grid.component.css']
 })
 export class UserGridComponent implements OnInit {
+  @Input() users: User[] = [];
 
   constructor() { }
 

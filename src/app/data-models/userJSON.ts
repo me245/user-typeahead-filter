e6005e4b7a7a1c38b30c74b6f4e1e6202214c9db
Michael Evanson <michael.evanson@gmail.com>
2018-03-10 22:56:59 -0700

    Add classes and helper methods for converting to/from json

---
+++ src/app/data-models/userJSON.ts
@@ -0,0 +1,9 @@
+export class UserJSON {
+  id: number = void 0;
+  email: string = void 0;
+  fname: string = void 0;
+  lname: string = void 0;
+  isCompany: number = void 0;
+  company?: string = void 0;
+  created: string = void 0;
+}

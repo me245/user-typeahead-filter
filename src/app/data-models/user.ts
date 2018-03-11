09cb3b6fcdecac5cc78a2f13420e7d636ab900a6
Michael Evanson <michael.evanson@gmail.com>
2018-03-11 08:04:29 -0600

    Clean up encoding/decoding

--- src/app/data-models/user.ts
+++ src/app/data-models/user.ts
@@ -1,3 +1,5 @@
+import { UserJSON } from './userJSON';
+
 export class User {
   id: number = void 0;
   email: string = void 0;
@@ -6,4 +8,27 @@ export class User {
   isCompany: boolean = void 0;
   company?: string = void 0;
   created: Date = void 0;
+
+  static fromJSON(json: UserJSON): User {
+    if (typeof json === 'string') {
+      return JSON.parse(json, User.reviver);
+    } else {
+      const user = Object.create(User.prototype);
+      return Object.assign(user, json, {
+        created: new Date(json.created),
+        isCompany: json.isCompany > 0 ? true : false
+      });
+    }
+  }
+
+  static reviver(key: string, value: any): any {
+    return key === '' ? User.fromJSON(value) : value;
+  }
+
+  toJSON(): UserJSON {
+    return Object.assign({}, this, {
+      created: this.created.toString(),
+      isCompany: this.isCompany ? 1 : 0
+    });
+  }
 }

29c6964b24ed5ec9dff25d49c29f4da5f4731010
Michael Evanson <michael.evanson@gmail.com>
2018-03-10 20:22:11 -0700

    add user model and user grid component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UserGridComponent } from './user-grid/user-grid.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserGridComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/data-models/user.ts
@@ -0,0 +1,9 @@
+export class User {
+  id: number = void 0;
+  email: string = void 0;
+  fname: string = void 0;
+  lname: string = void 0;
+  isCompany: boolean = void 0;
+  company?: string = void 0;
+  created: Date = void 0;
+}
